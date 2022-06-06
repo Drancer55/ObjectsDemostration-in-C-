e3e9aabd60f948e2aee14d29ac1e0fdf3fef0aec
Oscar Ruiz Antonio <86693936+Drancer55@users.noreply.github.com>
2022-06-06 13:33:02 -0500

    Employee, Salary employee & commission Employee finish

---
+++ POOConcepts/POOConcepts/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOConcepts
+{
+    public abstract class Employee
+    {
+        //get = obtener; set = asignar el valor
+        public int Id { get; set; }
+
+        public string FirstName //propiedad en CamelCase
+        { get; set; }
+
+        public string LastName { get; set; }
+
+        public Date BirthDay { get; set; }
+
+        public Date Hiringdate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public abstract decimal GetValueToPay();
+
+        public override string ToString()
+        {
+                return $"{Id} - {FirstName} {LastName}, " +
+                $"Birth: {BirthDay}, " +
+                $"Hiring: {Hiringdate}, " +
+                $"Is active: {IsActive}";
+        }
+    }
+}
